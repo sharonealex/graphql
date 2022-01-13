cf9f0c4816be4b51d0254346e712fbc1909f601b
Sharon <sharonelizalex@gmail.com>
2022-01-13 13:44:04 +1100

    added server code

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,6 @@
 const db = require('../config/connection');
 const { Profile } = require('../models');
-const profileSeeds = require('./profileSeeds.json');
+const profileSeeds = require('./profileSeed.json');
 
 db.once('open', async () => {
   try {
